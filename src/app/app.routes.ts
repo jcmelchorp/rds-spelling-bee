6471f1e5cadb5bfe30639cd360496d269ed0ac19
Julio C. Melchor P <jcmelchorp@gmail.com>
2025-03-08 00:32:24 -0600

    Agregar componente de landing y actualizar reglas de seguridad en la base de datos. Se mejora la estructura de rutas y se implementa un diseño responsivo.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,46 +10,53 @@ import { authGuard, publicGuard } from './core/auth/guards/auth.guard';
 import { RegisterComponent } from './core/auth/register/register.component';
 import { TermsComponent } from './core/auth/terms/terms.component';
 import { PrivacyPolicyComponent } from './core/auth/privacy-policy/privacy-policy.component';
+import { LandingComponent } from './core/layout/landing/landing.component';
 
 export const routes: Routes = [
-    {
+  {
+    path: '',
+    component: NavigationComponent,
+    children: [
+      {
         path: '',
-        component: NavigationComponent,
-        children: [
-            { 
-                path: '', component: HomeComponent 
-            },
-            {
-                path: 'terms',
-                component: TermsComponent,
-              },
-              {
-                path: 'policy',
-                component: PrivacyPolicyComponent,
-              },
-            { 
-                path: 'editor', 
-                canActivate: [authGuard],
-                component: WordlistsComponent 
-            },
-            {
-                path: 'login',
-                canActivate: [publicGuard],
-                component: LoginComponent,
-              },
-              {
-                path: 'register',
-                canActivate: [publicGuard],
-                component: RegisterComponent,
-              },
-            {
-                path: 'contest', 
-                canActivate: [authGuard],
-                loadComponent: () => import('./features/contest/contest/contest.component').then(m => m.ContestComponent)
-
-            },
-
-        ]
-    },
-    { path: '**', redirectTo: '/', pathMatch: 'full' }
+        component: LandingComponent,
+      },
+      {
+        path: 'home',
+        component: HomeComponent,
+      },
+      {
+        path: 'terms',
+        component: TermsComponent,
+      },
+      {
+        path: 'policy',
+        component: PrivacyPolicyComponent,
+      },
+      {
+        path: 'editor',
+        canActivate: [authGuard],
+        component: WordlistsComponent,
+      },
+      {
+        path: 'login',
+        canActivate: [publicGuard],
+        component: LoginComponent,
+      },
+      {
+        path: 'register',
+        canActivate: [publicGuard],
+        component: RegisterComponent,
+      },
+      {
+        path: 'contest',
+        canActivate: [authGuard],
+        loadComponent: () =>
+          import('./features/contest/contest/contest.component').then(
+            (m) => m.ContestComponent
+          ),
+      },
+    ],
+  },
+  { path: '**', redirectTo: '/', pathMatch: 'full' },
 ];
---
+++ src/app/core/layout/landing/landing.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-landing',
+  standalone:true,
+  imports:[
+    RouterLink,
+MatButtonModule
+  ],
+  templateUrl: './landing.component.html',
+  styleUrls: ['./landing.component.scss']
+})
+export class LandingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
