db61dacad40a4863899bf5f66418cdf88b0da7e4
Julio C. Melchor P <jcmelchorp@gmail.com>
2025-03-15 20:43:45 -0600

    Ajustar formato del componente principal y agregar MatCardModule en el componente de términos. Se mejora la estructura del HTML y se optimizan las importaciones.

--- src/app/core/auth/terms/terms.component.ts
+++ src/app/core/auth/terms/terms.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-terms',
   standalone:true,
-
+imports:[MatCardModule],
   templateUrl: './terms.component.html',
   styleUrls: ['./terms.component.scss']
 })
