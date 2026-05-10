deed52cd426f3f0e1d69fd21c3b42c52ba1ebf4b
Julio C. Melchor P <jcmelchorp@gmail.com>
2026-05-09 21:37:50 -0600

    improve animations

--- src/app/features/contest/wordlists/wordlists.component.ts
+++ src/app/features/contest/wordlists/wordlists.component.ts
@@ -1,25 +1,46 @@
-import { Component, inject } from "@angular/core";
+import { Component, inject, signal } from "@angular/core";
 import { WordlistsService } from "./wordlists.service";
-import {  Wordlist } from "../wordlist/wordlist.model";
+import {  Word, Wordlist } from "../wordlist/wordlist.model";
 import { NgForOf,AsyncPipe,DatePipe } from "@angular/common";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { MatCardModule } from "@angular/material/card";
 import { MatButtonModule } from "@angular/material/button";
 import { MatIconModule } from "@angular/material/icon";
 import { MatFormFieldModule } from "@angular/material/form-field";
-import { RouterLink } from "@angular/router";
+import {COMMA, ENTER} from '@angular/cdk/keycodes';
 import { take } from "rxjs";
+import { MatExpansionModule } from "@angular/material/expansion";
+import { MatChipOption, MatChipListbox, MatChipRow, MatChipEditedEvent, MatChipInputEvent } from "@angular/material/chips";
+import { LiveAnnouncer } from "@angular/cdk/a11y";
 
 @Component({
     selector: 'app-wordlists',
     standalone: true,
-    imports: [DatePipe,AsyncPipe,NgForOf, RouterLink ,ReactiveFormsModule, FormsModule,MatCardModule,MatButtonModule,MatIconModule,MatFormFieldModule],
+    styles: `
+    .background{
+      background: 
+    radial-gradient(black 3px, transparent 4px),
+    radial-gradient(black 3px, transparent 4px),
+    linear-gradient(#fff 4px, transparent 0),
+    linear-gradient(45deg, transparent 74px, transparent 75px, #a4a4a4 75px, #a4a4a4 76px, transparent 77px, transparent 109px),
+    linear-gradient(-45deg, transparent 75px, transparent 76px, #a4a4a4 76px, #a4a4a4 77px, transparent 78px, transparent 109px),
+  #fff;
+  background-size: 109px 109px, 109px 109px,100% 6px, 109px 109px, 109px 109px;
+  background-position: 54px 55px, 0px 0px, 0px 0px, 0px 0px, 0px 0px;
+    }`,  
+    imports: [AsyncPipe, NgForOf, ReactiveFormsModule, FormsModule, MatCardModule, MatButtonModule, MatIconModule, MatFormFieldModule, MatExpansionModule, MatChipListbox,MatChipOption,MatChipRow],
     templateUrl: './wordlists.component.html',
   })
   export class WordlistsComponent {
+    readonly panelOpenState = signal(false);
     private readonly wordlistService = inject(WordlistsService);
     wordlist: Wordlist={id: '', level: '', words: [],timestamp: Date.now()};
     wordlists$ = this.wordlistService.list();
+    readonly addOnBlur = true;
+    readonly separatorKeysCodes = [ENTER, COMMA] as const;
+    readonly words = signal<Word[]>([]);
+    readonly announcer = inject(LiveAnnouncer);
+
   
     async submit() {
       this.wordlistService
@@ -37,6 +58,7 @@ import { take } from "rxjs";
         });
     }
     
+   
     updateWordlist(wordlist: Wordlist) {
       this.wordlistService.update(wordlist.id!, wordlist);
     }
@@ -44,5 +66,51 @@ import { take } from "rxjs";
     deleteWordlist(wordlist: Wordlist) {
       this.wordlistService.delete(wordlist.id!);
     }
+
+    add(event: MatChipInputEvent): void {
+      const value = (event.value || '').trim();
+  
+      // Add our word
+      if (value) {
+        this.words.update(words => [...words, {label: value}]);
+      }
+  
+      // Clear the input value
+      event.chipInput!.clear();
+    }
+  
+    remove(word: Word): void {
+      this.words.update(words => {
+        const index = words.indexOf(word);
+        if (index < 0) {
+          return words;
+        }
+  
+        words.splice(index, 1);
+        this.announcer.announce(`Removed ${word.label}`);
+        return [...words];
+      });
+    }
+  
+    edit(word: Word, event: MatChipEditedEvent) {
+      const value = event.value.trim();
+  
+      // Remove word if it no longer has a name
+      if (!value) {
+        this.remove(word);
+        return;
+      }
+  
+      // Edit existing word
+      this.words.update(words => {
+        const index = words.indexOf(word);
+        if (index >= 0) {
+            words[index].label = value;
+          return [...words];
+        }
+        return words;
+      });
+    }
+  
   }
   
