c44292b8b197db7441089bd6a48e93e6334b54f1
Julio C. Melchor P <jcmelchorp@gmail.com>
2026-05-13 19:29:00 -0600

    return to normal css

--- src/app/features/contest/wordlists/wordlists.component.ts
+++ src/app/features/contest/wordlists/wordlists.component.ts
@@ -1,7 +1,7 @@
 import { Component, inject, signal } from "@angular/core";
 import { WordlistsService } from "./wordlists.service";
 import {  Word, Wordlist } from "../wordlist/wordlist.model";
-import { NgForOf,AsyncPipe,DatePipe } from "@angular/common";
+import { NgForOf, AsyncPipe, DatePipe, NgIf } from "@angular/common";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { MatCardModule } from "@angular/material/card";
 import { MatButtonModule } from "@angular/material/button";
@@ -12,23 +12,29 @@ import { take } from "rxjs";
 import { MatExpansionModule } from "@angular/material/expansion";
 import { MatChipOption, MatChipListbox, MatChipRow, MatChipEditedEvent, MatChipInputEvent } from "@angular/material/chips";
 import { LiveAnnouncer } from "@angular/cdk/a11y";
+import { MatProgressSpinner } from "@angular/material/progress-spinner";
 
 @Component({
     selector: 'app-wordlists',
     standalone: true,
-    styles: `
-    .background{
-      background: 
-    radial-gradient(black 3px, transparent 4px),
-    radial-gradient(black 3px, transparent 4px),
-    linear-gradient(#fff 4px, transparent 0),
-    linear-gradient(45deg, transparent 74px, transparent 75px, #a4a4a4 75px, #a4a4a4 76px, transparent 77px, transparent 109px),
-    linear-gradient(-45deg, transparent 75px, transparent 76px, #a4a4a4 76px, #a4a4a4 77px, transparent 78px, transparent 109px),
-  #fff;
-  background-size: 109px 109px, 109px 109px,100% 6px, 109px 109px, 109px 109px;
-  background-position: 54px 55px, 0px 0px, 0px 0px, 0px 0px, 0px 0px;
-    }`,  
-    imports: [AsyncPipe, NgForOf, ReactiveFormsModule, FormsModule, MatCardModule, MatButtonModule, MatIconModule, MatFormFieldModule, MatExpansionModule, MatChipListbox,MatChipOption,MatChipRow],
+  //   styles: `
+  //   .background{
+  //     height: calc(100% - 55px);
+  //     background: 
+  //   radial-gradient(black 3px, transparent 4px),
+  //   radial-gradient(black 3px, transparent 4px),
+  //   linear-gradient(#fff 4px, transparent 0),
+  //   linear-gradient(45deg, transparent 74px, transparent 75px, #a4a4a4 75px, #a4a4a4 76px, transparent 77px, transparent 109px),
+  //   linear-gradient(-45deg, transparent 75px, transparent 76px, #a4a4a4 76px, #a4a4a4 77px, transparent 78px, transparent 109px),
+  // #fff;
+  // background-size: 109px 109px, 109px 109px,100% 6px, 109px 109px, 109px 109px;
+  // background-position: 54px 55px, 0px 0px, 0px 0px, 0px 0px, 0px 0px;
+  //   }
+    
+  
+  //   `,  
+  styleUrls: ['./wordlists.component.scss'],
+    imports: [AsyncPipe, NgForOf, ReactiveFormsModule, FormsModule, MatCardModule, MatButtonModule, MatIconModule, MatFormFieldModule, MatExpansionModule, MatChipListbox, MatChipOption, MatChipRow, NgIf, MatProgressSpinner],
     templateUrl: './wordlists.component.html',
   })
   export class WordlistsComponent {
